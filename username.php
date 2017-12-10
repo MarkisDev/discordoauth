609d614ba429f4333e90587df239e1354049ce42
Dunkledore <b7dunkerley@me.com>
2017-12-10 23:03:35 +0000

    will simply return username

---
+++ username.php
@@ -0,0 +1,11 @@
+<?php
+	require_once('discord.php');
+	if (!array_key_exists('user', $_SESSION))
+	{
+		init("", 'INSERT_CALLBACK_URL','INSERT_CLIENT_ID', 'INSERT_CLIENT_SECRET');
+		get_user();
+		
+	}
+	echo $_SESSION['username']
+
+?>
