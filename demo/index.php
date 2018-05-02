4896edeafbafdece9ffd3fadde27708308016f59
Rijuth Menon <rijuthm@gmail.com>
2018-05-02 18:17:01 +0530

    Changed short tags to normal ones

--- demo/index.php
+++ demo/index.php
@@ -20,12 +20,12 @@
      <title> Demo Oauth </title>
      <h2 style="color : red;">A Simple Working Demo of the Script </h2>
      <h1> User Details :</h1>
-     <p> Name : <? echo $_SESSION['username'] . '#'. $_SESSION['discrim']; ?></p>
-     <p> ID : <? echo $_SESSION['user_id']; ?></p>
-     <p> Profile Picture : <img src="https://cdn.discordapp.com/avatars/<?echo $_SESSION['user_id']; ?>/<? echo $_SESSION['user_avatar']; $extention = is_animated($_SESSION['user_avatar']); echo $extention; ?>" /></p>
-     <p> Response : <? echo json_encode($_SESSION['response']); ?></p>
+     <p> Name : <?php echo $_SESSION['username'] . '#'. $_SESSION['discrim']; ?></p>
+     <p> ID : <?php echo $_SESSION['user_id']; ?></p>
+     <p> Profile Picture : <img src="https://cdn.discordapp.com/avatars/<?echo $_SESSION['user_id']; ?>/<?php echo $_SESSION['user_avatar']; $extention = is_animated($_SESSION['user_avatar']); echo $extention; ?>" /></p>
+     <p> Response : <?php echo json_encode($_SESSION['response']); ?></p>
      <br />
      <h1> User Guilds :</h1>
-     <p> <? echo json_encode($_SESSION['guilds']); ?></p>
-     <h3 style="color:purple;"><a href="<? echo url("YOUR CLIENT ID", "YOUR REDIRCT URI", "YOUR SCOPE(S) SEPARATED BY A SPACE"); ?>">Oauth Link </a></h3>
+     <p> <?php echo json_encode($_SESSION['guilds']); ?></p>
+     <h3 style="color:purple;"><a href="<?php echo url("YOUR CLIENT ID", "YOUR REDIRCT URI", "YOUR SCOPE(S) SEPARATED BY A SPACE"); ?>">Oauth Link </a></h3>
  </html>
