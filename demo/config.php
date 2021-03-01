48f72221cdab7ccee54e8d8958385a204760fb9d
Rijuth Menon <rijuthm@gmail.com>
2021-03-01 19:20:12 +0530

    Added bot token to config

--- demo/config.php
+++ demo/config.php
@@ -1,18 +1,28 @@
 <?php
+
 # CLIENT ID
-# https://prnt.sc/yohgfs (screenshot)
+# https://i.imgur.com/GHI2ts5.png (screenshot)
 $client_id = "";
 
 # CLIENT SECRET
-# https://prnt.sc/yohgvs (screenshot)
+# https://i.imgur.com/r5dYANR.png (screenshot)
 $secret_id = "";
 
 # SCOPES SEPARATED BY SPACE
-# example: identify guilds
+# example: identify guilds 
 $scopes = "";
 
 # REDIRECT URL
-# example: https://mydomian.com/includes/login.php
-# example: https://mydomian.com/test/includes/login.php
+# example: https://mydomain.com/includes/login.php
+# example: https://mydomain.com/test/includes/login.php
 $redirect_url = "";
+
+# IMPORTANT READ THIS:
+# - Set the `$bot_token` to your bot token if you want to use guilds.join scope to add a member to your server
+# - Check login.php for more detailed info on this.
+# - Leave it as it is if you do not want to use 'guilds.join' scope.
+
+# https://i.imgur.com/2tlOI4t.png (screenshot)
+$bot_token = null;
+
 ?>
