c6641cf039f763f1914f3b37b6a5aa8363f4a74a
GeoCyberwolf <geostar@outlook.com>
2022-08-08 16:51:26 +0100

    added remove member on cancellation functionality

--- mm-discord-oauth.php
+++ mm-discord-oauth.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Breakthrough Discord Oauth
  * Description:       Provides "Login with Discord" via a shortcode, then uses the user's oauth token to get their discord username, populate the field on the member profile and add them to the server after validating their membership.
  * Plugin URI:        https://github.com/BreakthroughParty
- * Version:           0.31-dev
+ * Version:           0.42-dev
  * Author:            Breakthrough Contributors
  * Author URI:        https://breakthroughparty.org.uk/
  */
@@ -17,6 +17,8 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-content/plugins/membermouse/includes/init.php");
 	require_once("resources/config.php");
 	require_once("resources/discord.php");
+	require_once("resources/settingspage.php");
+	$settings = get_option('discord_oauth_plugin_options');
  add_shortcode( 'join_discord', function ( $atts ) {
 	#$atts = shortcode_atts( array(
 	#	'foo' => 'no foo'
@@ -40,138 +42,75 @@
     return wp_kses_post("<a href='{$auth_url}' class='nobg'><img src='/wp-content/plugins/mm-discord-oauth/resources/discord_button.png'></img></a>");
 }
 );
-function discord_oauth_add_settings_page() {
-    add_options_page( 'Discord OAuth', 'Discord OAuth', 'manage_options', 'mm-discord-oauth', 'discord_oauth_render_plugin_settings_page' );
-}
-add_action( 'admin_menu', 'discord_oauth_add_settings_page' );
-function discord_oauth_render_plugin_settings_page() {
-    ?>
-    <h2>Discord oAuth for MemberMouse Settings</h2>
-    <form action="options.php" method="post">
-        <?php 
-        settings_fields( 'discord_oauth_plugin_options' );
-        do_settings_sections( 'mm_discord_oauth' ); ?>
-        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
-    </form>
-    <?php
-}
-function discord_oauth_register_settings() {
-    register_setting( 'discord_oauth_plugin_options', 'discord_oauth_plugin_options', 'discord_oauth_plugin_options_validate' );
-    add_settings_section( 'discord_api_settings', 'Discord API Settings', 'discord_oauth_dapi_section_text', 'mm_discord_oauth' );
-	add_settings_section( 'discord_server_settings', 'Discord Server Settings', 'discord_oauth_dserver_section_text', 'mm_discord_oauth' );
-	add_settings_section( 'mm_api_settings', 'MemberMouse API Settings', 'discord_oauth_mmapi_section_text', 'mm_discord_oauth' );
-	add_settings_section( 'site_settings', 'Your Site Settings', 'discord_oauth_site_section_text', 'mm_discord_oauth' );
-	//discord settings
-    add_settings_field( 'discord_oauth_setting_client_id', 'Client ID', 'discord_oauth_setting_client_id', 'mm_discord_oauth', 'discord_api_settings' );
-    add_settings_field( 'discord_oauth_setting_client_secret', 'Client Secret', 'discord_oauth_setting_client_secret', 'mm_discord_oauth', 'discord_api_settings' );
-	add_settings_field( 'discord_oauth_setting_bot_token', 'Bot Token', 'discord_oauth_setting_bot_token', 'mm_discord_oauth', 'discord_api_settings' );
-	//discord server settings
-	add_settings_field( 'discord_oauth_setting_guild_id', 'Discord Guild/Server ID', 'discord_oauth_setting_guild_id', 'mm_discord_oauth', 'discord_server_settings' );
-	add_settings_field( 'discord_oauth_setting_role_id', 'Discord Role ID to Grant', 'discord_oauth_setting_role_id', 'mm_discord_oauth', 'discord_server_settings' );
-	//mm settings
-	add_settings_field( 'discord_oauth_setting_mm_key', 'MemberMouse API Key', 'discord_oauth_setting_mm_key', 'mm_discord_oauth', 'mm_api_settings' );
-	add_settings_field( 'discord_oauth_setting_mm_secret', 'MemberMouse Secret', 'discord_oauth_setting_mm_secret', 'mm_discord_oauth', 'mm_api_settings' );
-	//site settings
-	add_settings_field( 'discord_oauth_setting_login_url', 'Login Page URL', 'discord_oauth_setting_login_url', 'mm_discord_oauth', 'site_settings' );
-	add_settings_field( 'discord_oauth_setting_member_url', 'Member Area URL', 'discord_oauth_setting_member_url', 'mm_discord_oauth', 'site_settings' );
-	add_settings_field( 'discord_oauth_setting_success_url', 'Success Page URL', 'discord_oauth_setting_success_url', 'mm_discord_oauth', 'site_settings' );
-}
-add_action( 'admin_init', 'discord_oauth_register_settings' );
+$bot_token = $settings["bot_token"];
+$guildid = $settings['guild_id'];
+$mmkey = $settings['mm_key'];
+$mmsecret = $settings['mm_secret'];
+$wpurl = get_site_url();
+define("BOT_TOKEN",$bot_token);
+function discord_notify($options) {
+	$msgobj=json_encode($options, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+	$dAPI_SendMessage = "https://discordapp.com/api/channels/965003713355845635/messages";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,
+		array( "Authorization: Bot " . BOT_TOKEN,
+				'Content-Type: application/json',
+				'Referer: https://discordapp.com/channels/@me'
+		));
 
-// validate settings
+	curl_setopt_array( $ch, [
+		CURLOPT_URL => $dAPI_SendMessage,
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => $msgobj]);
+		
+	$msgresponse = curl_exec( $ch );
+	curl_close( $ch );
+	return $msgresponse;
+}
 
-function discord_oauth_plugin_options_validate( $input ) {
-	$validatedinput = $input;
-	if ( ! preg_match( '/[0-9]{18}/', $validatedinput['client_id'] ) ) {
-		$validatedinput['client_id'] = '';
-		add_settings_error('discord_oauth_setting_client_id', 'client_id_error', 'Invalid Discord Client ID. Client ID should be 18 numbers.', 'error');
+function UncheckDiscordLinked($memberid) {
+	global $mmkey, $mmsecret, $wpurl;
+    $inputParams = "apikey={$mmkey}&apisecret={$mmsecret}&";
+    $inputParams .= "member_id={$memberid}&"; 
+	$inputParams .= "custom_field_9=mm_cb_off";
+    $apiCallUrl = "{$wpurl}/wp-content/plugins/membermouse/api/request.php?q=/updateMember";
+    $ch = curl_init($apiCallUrl); 
+
+    curl_setopt($ch, CURLOPT_POST, 1); 
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $inputParams); 
+    curl_setopt($ch, CURLOPT_HEADER, 0); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    $result = curl_exec($ch); 
+    curl_close($ch);
+}
+function RemoveMemberFromDiscord($data) {
+	global $guildid;
+	$memberstatus = $data["status"];
+	$discorduserid = $data["cf_10"];
+	$discordname = $data["cf_7"];
+	$memberid = $data["member_id"];
+	//if status is cancelled(2) or expired(8)
+	if ($memberstatus == 2 || $memberstatus == 8) {
+		//if we have the discord user ID
+		if (!empty($discorduserid)) {
+			UncheckDiscordLinked($memberid);
+			$response = kick_user($guildid, $discorduserid);
+			$fullresponse = var_dump($response);
+			$msgobj = [
+			"content" => "Attempting Kick Discord User ID: {$discorduserid} Discord API Says: {$fullresponse}",
+		];
+		$m=discord_notify($msgobj);
+			
+		}
+		elseif (empty($discorduserid)) {
+			$msgobj = [
+			"content" => "No discord ID for cancelled member {$memberid}. They will need to be kicked manually! If we have a username for them it is: {$discordname}",
+		];
+		$m=discord_notify($msgobj);
+		}
+		
 	}
-	if ( ! preg_match( '/[a-zA-Z0-9]{32}/', $validatedinput['client_secret'] ) ) {
-		$validatedinput['client_secret'] = '';
-		add_settings_error('discord_oauth_setting_client_secret', 'client_secret_error', 'Invalid Discord Client Secret. Client Secret should be 32 alphanumeric characters.', 'error');
-	}
-	
-	
-	
-	return $validatedinput;
-	
-	}
-
-// Settings form
-
-//discord api section
-function discord_oauth_dapi_section_text() {
-	global $redirect_url;
-    echo "<p>Set the Discord API credentials here.</p><p><b>Please Note:</b> You must add <b>{$redirect_url}</b> to the Redirects section<br>in the OAuth2 tab of the Discord Developer Portal or Discord oAuth will fail.";
 }
-
-function discord_oauth_setting_client_id() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_client_id' size='70' name='discord_oauth_plugin_options[client_id]' type='text' value='" . esc_attr( $options['client_id'] ) . "' />";
-}
-
-function discord_oauth_setting_client_secret() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_client_secret' size='70' name='discord_oauth_plugin_options[client_secret]' type='text' value='" . esc_attr( $options['client_secret'] ) . "' />";
-}
-
-function discord_oauth_setting_bot_token() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_bot_token' size='70' name='discord_oauth_plugin_options[bot_token]' type='text' value='" . esc_attr( $options['bot_token'] ) . "' />";
-}
-
-//discord server section
-function discord_oauth_dserver_section_text() {
-    echo '<p>Set the Discord server settings here.</p>';
-}
-
-function discord_oauth_setting_guild_id() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_guild_id' size='70' name='discord_oauth_plugin_options[guild_id]' type='text' value='" . esc_attr( $options['guild_id'] ) . "' />";
-}
-
-function discord_oauth_setting_role_id() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_role_id' size='70' name='discord_oauth_plugin_options[role_id]' type='text' value='" . esc_attr( $options['role_id'] ) . "' />";
-}
-
-//mm api section
-function discord_oauth_mmapi_section_text() {
-    echo '<p>Set the MemberMouse API credentials here.</p>';
-}
-
-function discord_oauth_setting_mm_key() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_mm_key' size='70' name='discord_oauth_plugin_options[mm_key]' type='text' value='" . esc_attr( $options['mm_key'] ) . "' />";
-}
-
-function discord_oauth_setting_mm_secret() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-    echo "<input id='discord_oauth_setting_mm_secret' size='70' name='discord_oauth_plugin_options[mm_secret]' type='text' value='" . esc_attr( $options['mm_secret'] ) . "' />";
-}
-
-//site settings section
-function discord_oauth_site_section_text() {
-    echo '<p>Set your site settings here</p>';
-}
-
-function discord_oauth_setting_login_url() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-	echo '<p>Location of the MemberMouse Login page. Anyone not logged in will be sent here if they somehow hit the discord login script.</p>';
-    echo "<input id='discord_oauth_setting_login_url' size='70' name='discord_oauth_plugin_options[login_url]' type='text' value='" . esc_attr( $options['login_url'] ) . "' />";
-}
-
-function discord_oauth_setting_member_url() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-	echo '<p>Location of the MemberMouse Member Home page. Users will be sent here if they cancel the Discord permissions prompt.</p>';
-    echo "<input id='discord_oauth_setting_member_url' size='70' name='discord_oauth_plugin_options[member_url]' type='text' value='" . esc_attr( $options['member_url'] ) . "' />";
-}
-
-function discord_oauth_setting_success_url() {
-    $options = get_option( 'discord_oauth_plugin_options' );
-	echo '<p>Location of the success page. Users will be sent here after successfully authenticating with Discord and joining the server.</p>';
-    echo "<input id='discord_oauth_setting_success_url' size='70' name='discord_oauth_plugin_options[success_url]' type='text' value='" . esc_attr( $options['success_url'] ) . "' />";
-}
-
-
+add_action('mm_member_status_change', 'RemoveMemberFromDiscord');
 ?>
