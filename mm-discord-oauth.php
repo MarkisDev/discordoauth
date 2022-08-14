157475b2307d282ac2bdca63f6a675e324816e32
GeoCyberwolf <geostar@outlook.com>
2022-08-14 17:02:35 +0100

    updated plugin uri to point to this repo

--- mm-discord-oauth.php
+++ mm-discord-oauth.php
@@ -7,7 +7,7 @@
  * @wordpress-plugin
  * Plugin Name:       MemberMouse Discord Oauth
  * Description:       Provides "Login with Discord" via a shortcode, then uses the user's oauth token to get their discord username, populate the field on the member profile and add them to the server after validating their membership.
- * Plugin URI:        https://github.com/BreakthroughParty
+ * Plugin URI:        https://github.com/BreakthroughParty/mm-discord-oauth
  * Version:           0.45
  * Author:            Breakthrough Tech Team
  * Author URI:        https://breakthroughparty.org.uk/
