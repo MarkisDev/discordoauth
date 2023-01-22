32584a6f177328abdfeb118622a9687017785702
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2023-01-22 15:27:01 +0100

    Updated discordauith with classes
    Now it's tidier, easier and more intuitive to use

--- discord.php
+++ discord.php
@@ -3,175 +3,149 @@
  * This file contains the core functions of the oauth2 script.
  * @author : MarkisDev
  * @copyright : https://markis.dev
- */
+ * @contributor: FoxWorn3365
+*/
 
-# Starting session so we can store all the variables
-session_start();
+namespace Discord {
+  class Auth {
+    protected string $baseurl = "https://discord.com";
+    protected string $bot_token;
+    protected mixed $config;
 
-# Setting the base url for API requests
-$GLOBALS['base_url'] = "https://discord.com";
-
-# Setting bot token for related requests
-$GLOBALS['bot_token'] = null;
-
-# A function to generate a random string to be used as state | (protection against CSRF)
-function gen_state()
-{
-    $_SESSION['state'] = bin2hex(openssl_random_pseudo_bytes(12));
-    return $_SESSION['state'];
-}
-
-# A function to generate oAuth2 URL for logging in
-function url($clientid, $redirect, $scope)
-{
-    $state = gen_state();
-    return 'https://discordapp.com/oauth2/authorize?response_type=code&client_id=' . $clientid . '&redirect_uri=' . $redirect . '&scope=' . $scope . "&state=" . $state;
-}
-
-# A function to initialize and store access token in SESSION to be used for other requests
-function init($redirect_url, $client_id, $client_secret, $bot_token = null)
-{
-    if ($bot_token != null)
-        $GLOBALS['bot_token'] = $bot_token;
-    $code = $_GET['code'];
-    $state = $_GET['state'];
-    # Check if $state == $_SESSION['state'] to verify if the login is legit | CHECK THE FUNCTION get_state($state) FOR MORE INFORMATION.
-    $url = $GLOBALS['base_url'] . "/api/oauth2/token";
-    $data = array(
-        "client_id" => $client_id,
-        "client_secret" => $client_secret,
-        "grant_type" => "authorization_code",
-        "code" => $code,
-        "redirect_uri" => $redirect_url
-    );
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    $_SESSION['access_token'] = $results['access_token'];
-}
-
-# A function to get user information | (identify scope)
-function get_user($email = null)
-{
-    $url = $GLOBALS['base_url'] . "/api/users/@me";
-    $headers = array('Content-Type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $_SESSION['access_token']);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    $_SESSION['user'] = $results;
-    $_SESSION['username'] = $results['username'];
-    $_SESSION['discrim'] = $results['discriminator'];
-    $_SESSION['user_id'] = $results['id'];
-    $_SESSION['user_avatar'] = $results['avatar'];
-    # Fetching email 
-    if ($email == True) {
-        $_SESSION['email'] = $results['email'];
+    function __construct(array $config) {
+      $this->config = (object)$config;
+      $this->config->baseurl = $this->baseurl;
     }
-}
 
-# A function to give roles to the user
-# Note : The bot has to be a member of the server with MANAGE_ROLES permission.
-#        The bot DOES NOT have to be online, just has to be a bot application and has to be a member of the server.
-#        This is the basic function which requires few parameters. [ 1: Guild ID,  2: Role ID ]
-function give_role($guildid, $roleid)
-{
-    $data = json_encode(array("roles" => array("$roleid")));
-    $url = $GLOBALS['base_url'] . "/api/guilds/$guildid/members/" . $_SESSION['user_id'] . "/roles/$roleid";
-    $headers = array('Content-Type: application/json', 'Authorization: Bot ' . $GLOBALS['bot_token']);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    return $results;
-}
-
-# A function to get user guilds | (guilds scope)
-function get_guilds()
-{
-    $url = $GLOBALS['base_url'] . "/api/users/@me/guilds";
-    $headers = array('Content-Type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $_SESSION['access_token']);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    return $results;
-}
-
-# A function to fetch information on a single guild | (requires bot token)
-function get_guild($id)
-{
-    $url = $GLOBALS['base_url'] . "/api/guilds/$id";
-    $headers = array('Content-Type: application/x-www-form-urlencoded', 'Authorization: Bot ' . $GLOBALS['bot_token']);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    return $results;
-}
-
-# A function to get user connections | (connections scope)
-function get_connections()
-{
-    $url = $GLOBALS['base_url'] . "/api/users/@me/connections";
-    $headers = array('Content-Type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $_SESSION['access_token']);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    return $results;
-}
-
-# Function to make user join a guild | (guilds.join scope)
-# Note : The bot has to be a member of the server with CREATE_INSTANT_INVITE permission.
-#        The bot DOES NOT have to be online, just has to be a bot application and has to be a member of the server.
-#        This is the basic function with no parameters, you can build on this to give the user a nickname, mute, deafen or assign a role.      
-function join_guild($guildid)
-{
-    $data = json_encode(array("access_token" => $_SESSION['access_token']));
-    $url = $GLOBALS['base_url'] . "/api/guilds/$guildid/members/" . $_SESSION['user_id'];
-    $headers = array('Content-Type: application/json', 'Authorization: Bot ' . $GLOBALS['bot_token']);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $results = json_decode($response, true);
-    return $results;
-}
-
-# A function to verify if login is legit
-function check_state($state)
-{
-    if ($state == $_SESSION['state']) {
-        return true;
-    } else {
-        # The login is not valid, so you should probably redirect them back to home page
-        return false;
+    // Generate random string for state (for CSRF)
+    protected static function state() {
+      return bin2hex(openssl_random_pseudo_bytes(12));
     }
+
+    // Go
+    public function go(string $getState = null) {
+      // use config from this
+      $code = $_GET['code'];
+      $state = $_GET['state'];
+      if ($state !== NULL && $state !== $getState) {
+        return;
+      }
+      $url = $this->baseurl . "/api/oauth2/token";
+      $data = http_build_query(array(
+          "client_id" => $this->config->client_id,
+          "client_secret" => $this->config->client_secret,
+          "grant_type" => "authorization_code",
+          "code" => $code,
+          "redirect_uri" => $this->config->redirect_url
+      ));
+      $options = stream_context_create(array( 
+        'http' => array(
+          'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+          'method' => 'POST',
+          'content' => $data
+        )
+      )); 
+      return new \Discord\PromiseManager(json_decode(file_get_contents($url, false, $options))->access_token, $this->config);
+    }
+  }
+
+  class PromiseManager {
+    public int $status;
+    public string $token;
+    public string $user;
+    public mixed $config;
+
+    function __construct(string $token, mixed $config) {
+      $this->token = $token;
+      $this->config = (object)$config;
+    }
+
+    public function then(mixed $callback) {
+      return $callback((new \Discord\Get($this->token, $this->config)), (new \Discord\Set($this->token, $this->config)));
+    }
+ 
+    public function done(mixed $callback) {
+      $this->then($callback);
+    }
+  }
+
+  class Get {
+    protected string $token;
+    public mixed $config;
+
+    function __construct(string $token, mixed $config) {
+      $this->token = $token;
+      $this->config = (object)$config;
+    }
+
+    public function user() {
+      $url = $this->config->baseurl . "/api/users/@me";
+      $options = stream_context_create([ 
+        'http' => [
+          'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                      "Authorization: Bearer {$this->token}\r\n",
+          'method' => 'GET'
+        ]
+      ]);
+      return json_decode(file_get_contents($url, false, $options));
+    }
+
+    public function guilds() {
+      $url = $this->config->baseurl . "/api/users/@me/guilds";
+      $options = stream_context_create([ 
+        'http' => [
+          'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                      "Authorization: Bearer {$this->token}\r\n",
+          'method' => 'GET'
+        ]
+      ]);
+      return json_decode(file_get_contents($url, false, $options));
+    }
+
+    public function guild(int $id) {
+      $url = $this->config->baseurl . "/api/guilds/{$id}";
+      $options = stream_context_create([ 
+        'http' => [
+          'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                      "Authorization: Bearer {$this->token}\r\n",
+          'method' => 'GET'
+        ]
+      ]);
+      return json_decode(file_get_contents($url, false, $options));
+    }
+
+    public function connections() {
+      $url = $this->config->baseurl . "/api/users/@me/connections";
+      $options = stream_context_create([ 
+        'http' => [
+          'header' => "Content-Type: application/x-www-form-urlencoded\r\n" .
+                      "Authorization: Bearer {$this->token}\r\n",
+          'method' => 'GET'
+        ]
+      ]);
+      return json_decode(file_get_contents($url, false, $options));
+    }
+  }
+  
+  class Set {
+    public string $token;
+    public object $config;
+
+    function __construct(string $token, mixed $config) {
+      $this->token = $token;
+      $this->config = (object)$config;
+    }
+
+    public function role(int $guild, int $user, int $role) {
+      $url = $this->config->baseurl . "/api/guilds/{$guild}/members/{$user}/roles/{$role}";
+      $options = stream_context_create([ 
+        'http' => [
+          'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                      "Authorization: Bearer {$this->token}\r\n",
+          'method' => 'PUT'
+        ]
+      ]);
+      return json_decode(file_get_contents($url, false, $options));
+    }
+  }
 }
